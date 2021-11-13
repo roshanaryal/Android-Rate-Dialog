a151c69698d243f2fe7115238cc3e4371c742abb
Roshan Aryal <roshanar.dev@gmail.com>
2021-11-13 15:44:16 +0545

    v1.0.0 fixed bug

--- app/src/main/java/com/aarappstudios/androidrate/MainActivity.java
+++ app/src/main/java/com/aarappstudios/androidrate/MainActivity.java
@@ -1,8 +1,8 @@
 package com.aarappstudios.androidrate;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.aarappstudios.androidratedialog.AppRate;
 
